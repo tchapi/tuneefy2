a415730bda3a921244d93ad5dffb89eccc5d59a6
tchapi <regbasket@gmail.com>
2015-04-19 21:01:41 +0200

    Rdio is now functional

--- src/tuneefy/Platform/Platforms/RdioPlatform.hh
+++ src/tuneefy/Platform/Platforms/RdioPlatform.hh
@@ -31,29 +31,29 @@ class RdioPlatform extends Platform implements WebStreamingPlatformInterface
    // Platform::SEARCH_ARTIST => self::API_ENDPOINT
   };
   protected ImmMap<int,?string> $terms = ImmMap {
-    Platform::LOOKUP_TRACK  => "keys",
-    Platform::LOOKUP_ALBUM  => "keys",
-    Platform::LOOKUP_ARTIST => "keys",
+    Platform::LOOKUP_TRACK  => "url",
+    Platform::LOOKUP_ALBUM  => "url",
+    Platform::LOOKUP_ARTIST => "url",
     Platform::SEARCH_TRACK  => "query",
     Platform::SEARCH_ALBUM  => "query",
    // Platform::SEARCH_ARTIST => "query" 
   };
   protected ImmMap<int,ImmMap<string,mixed>> $options = ImmMap {
-    Platform::LOOKUP_TRACK  => ImmMap { "method" => "get", "extras" => "-*,name,album,artist,icon,type" }, // type = t
-    Platform::LOOKUP_ALBUM  => ImmMap { "method" => "get", "extras" => "-*,name,artist,icon,type" }, // type = a 
-    Platform::LOOKUP_ARTIST => ImmMap { "method" => "get", "extras" => "-*,name,icon,type" }, // type = r
+    Platform::LOOKUP_TRACK  => ImmMap { "method" => "getObjectFromUrl", "extras" => "-*,name,album,artist,icon,type" }, // type = t
+    Platform::LOOKUP_ALBUM  => ImmMap { "method" => "getObjectFromUrl", "extras" => "-*,name,artist,icon,type" }, // type = a 
+    Platform::LOOKUP_ARTIST => ImmMap { "method" => "getObjectFromUrl", "extras" => "-*,name,icon,type" }, // type = r
     Platform::SEARCH_TRACK  => ImmMap { "method" => "search", "types" => "Track", "count" => Platform::LIMIT },
     Platform::SEARCH_ALBUM  => ImmMap { "method" => "search", "types" => "Album", "count" => Platform::LIMIT },
    // Platform::SEARCH_ARTIST => ImmMap { "method" => "search", "types" => "Artist", "count" => Platform::LIMIT } 
   };
 
-  // http://open.spotify.com/track/5jhJur5n4fasblLSCOcrTp
-  const string REGEX_SPOTIFY_ALL = "/(artist|album|track)(:|\/)(?P<item_id>[a-zA-Z0-9]*)$/";
+  // http://www.rdio.com/artist/The_Rolling_Stones/
+  // http://www.rdio.com/#/artist/David_Myhr/album/Soundshine
+  // http://www.rdio.com/artist/Kaskade/album/Never_Sleep_Alone/track/Never_Sleep_Alone/
+  const string REGEX_RDIO_TRACK = "/artist\/(?P<artist_slug>".Platform::REGEX_FULLSTRING.")\/album\/(?P<album_slug>".Platform::REGEX_FULLSTRING.")\/track\/(?P<track_slug>".Platform::REGEX_FULLSTRING.")[\/]?$/";
+  const string REGEX_RDIO_ALBUM = "/artist\/(?P<artist_slug>".Platform::REGEX_FULLSTRING.")\/album\/(?P<album_slug>".Platform::REGEX_FULLSTRING.")[\/]?$/";
+  const string REGEX_RDIO_ARTIST = "/artist\/(?P<artist_slug>".Platform::REGEX_FULLSTRING.")[\/]?$/";
     
-  // LOCAL files : http://open.spotify.com/local/hang+the+bastard/raw+sorcery/doomed+fucking+doomed/206
-  const string REGEX_SPOTIFY_LOCAL = "/local\/(?P<artist_name>".Platform::REGEX_FULLSTRING.")\/(?P<album_name>".Platform::REGEX_FULLSTRING.")\/(?P<track_name>".Platform::REGEX_FULLSTRING.")\/[0-9]+$/";
-    
-
   public function hasPermalink(string $permalink): bool
   {
     return (strpos($permalink, "rdio.") !== false);
@@ -61,11 +61,94 @@ class RdioPlatform extends Platform implements WebStreamingPlatformInterface
 
   public function expandPermalink(string $permalink, int $mode): ?PlatformResult
   {
-    return null;
+
+    $musical_entity = null;
+    $query_words = Vector {$permalink};
+
+    $match = Map {};
+
+    if (preg_match(self::REGEX_RDIO_TRACK, $permalink, $match)) {
+
+      $response = $this->fetchSync(Platform::LOOKUP_TRACK, $permalink);
+
+      if ($response === null || $response->data->status === "error") { return null; }
+
+      $entity = $response->data->result;
+      $musical_entity = new TrackEntity($entity->name, new AlbumEntity($entity->album, $entity->artist, $entity->icon)); 
+      $musical_entity->addLink($permalink);
+
+      $query_words = Vector {$entity->artist, $entity->name};
+      
+    } else if (preg_match(self::REGEX_RDIO_ALBUM, $permalink, $match)) {
+     
+      $response = $this->fetchSync(Platform::LOOKUP_ALBUM, $permalink);
+
+      if ($response === null || $response->data->status === "error") { return null; }
+
+      $entity = $response->data->result;
+      $musical_entity = new AlbumEntity($entity->name, $entity->artist, $entity->icon);
+      $musical_entity->addLink($permalink);
+
+      $query_words = Vector {$entity->artist, $entity->name};
+      
+    } else if (preg_match(self::REGEX_RDIO_ARTIST, $permalink, $match)) {
+
+      $response = $this->fetchSync(Platform::LOOKUP_ARTIST, $permalink);
+
+      if ($response === null || $response->data->status === "error") { return null; }
+      
+      $query_words = Vector {$response->data->result->name};
+
+    }
+  
+    // Consolidate results
+    $metadata = Map {"query_words" => $query_words};
+
+    if ($musical_entity !== null) {
+      $metadata->add(Pair {"platform", $this->getName()});
+    }
+
+    return new PlatformResult($metadata, $musical_entity);
+      
   }
 
   public async function search(int $type, string $query, int $limit, int $mode): Awaitable<?Vector<PlatformResult>>
   {
-    return null;
+
+    $response = await $this->fetch($type, $query);
+
+    if ($response === null || intval($response->data->result->results) === 0) {
+      return null;
+    }
+    $entities = $response->data->result->results;
+
+    // We actually don't pass the limit to the fetch() 
+    // request since it's not really useful, in fact
+    $length = min(count($entities), $limit?$limit:Platform::LIMIT);
+    
+    $musical_entities = Vector {};
+
+    // Normalizing each track found
+    for($i=0; $i<$length; $i++){
+    
+      $current_item = $entities[$i];
+
+      if ($type === Platform::SEARCH_TRACK) {
+        
+        $musical_entity = new TrackEntity($current_item->name, new AlbumEntity($current_item->album, $current_item->artist, $current_item->icon)); 
+        $musical_entity->addLink($current_item->shortUrl);
+             
+      } else /*if ($type === Platform::SEARCH_ALBUM)*/ {
+
+        $musical_entity = new AlbumEntity($current_item->name, $current_item->artist, $current_item->icon); 
+        $musical_entity->addLink($current_item->shortUrl);
+      
+      }
+      
+      $musical_entities->add(new PlatformResult(Map {"score" => Utils::indexScore($i)}, $musical_entity));
+
+    }
+    
+    return $musical_entities;
   }
 }
