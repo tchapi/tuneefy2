ec3ac0fb84b8e948978293c603da1c0952844c58
tchapi <regbasket+github@gmail.com>
2017-05-03 19:46:22 +0200

    Remove unused comment

--- src/tuneefy/Platform/ScrobblingPlatformInterface.php
+++ src/tuneefy/Platform/ScrobblingPlatformInterface.php
@@ -7,6 +7,4 @@ interface ScrobblingPlatformInterface
     public function hasPermalink(string $permalink): bool;
 
     public function expandPermalink(string $permalink, int $mode);
-
- //: ?PlatformResult;
 }
--- src/tuneefy/Platform/WebStreamingPlatformInterface.php
+++ src/tuneefy/Platform/WebStreamingPlatformInterface.php
@@ -7,6 +7,4 @@ interface WebStreamingPlatformInterface
     public function hasPermalink(string $permalink): bool;
 
     public function expandPermalink(string $permalink, int $mode);
-
- //: ?PlatformResult;
 }
