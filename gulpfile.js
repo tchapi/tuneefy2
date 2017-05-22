1486c96ead077266205a76b906f85c1296a9be7b
tchapi <regbasket+github@gmail.com>
2017-05-22 21:35:47 +0200

    Use gulp and tools for css/js

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var sass = require('gulp-sass');
+var pump = require('pump');
+
+let resourcesFolder = 'src/tuneefy/Resources/';
+let webFolder = 'web/'
+
+gulp.task('javascript', function () {
+  pump([
+        gulp.src(resourcesFolder + 'js/**/*.js'),
+        uglify(),
+        gulp.dest(webFolder + 'js')
+    ]);
+});
+
+gulp.task('sass', function () {
+  pump([
+        gulp.src(resourcesFolder + 'scss/styles.scss'),
+        sass({outputStyle: 'compressed'}).on('error', sass.logError),
+        gulp.dest(webFolder + 'css')
+    ]);
+});
+
+gulp.task('default', ['sass', 'javascript']);
