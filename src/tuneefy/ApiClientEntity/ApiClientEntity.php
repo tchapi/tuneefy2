d3662ef9d90b04430ae3e88ecc9294cf6048f3c5
tchapi <regbasket+github@gmail.com>
2017-08-04 18:51:24 +0200

    Improve ApiClientEntity

--- src/tuneefy/ApiClientEntity/ApiClientEntity.php
+++ src/tuneefy/ApiClientEntity/ApiClientEntity.php
@@ -15,4 +15,59 @@ class ApiClientEntity
 
     private $createdAt;
 
+    // Getters and setters
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): this
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): this
+    {
+        $this->email = $email;
+        return $this;
+    }
+
+    public function getUrl(): string
+    {
+        return $this->url;
+    }
+
+    public function setUrl(string $url): this
+    {
+        $this->url = $url;
+        return $this;
+    }
+
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): this
+    {
+        $this->description = $description;
+        return $this;
+    }
+
+    public function getCreatedAt(): \DateTime
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTime $createdAt): this
+    {
+        $this->createdAt = clone $createdAt;
+        return $this;
+    }
 }
