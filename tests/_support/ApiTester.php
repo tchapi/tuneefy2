df0192f8005cb0fd9c62b43ddf85f31e9ee7c4cd
tchapi <regbasket+github@gmail.com>
2018-02-21 09:02:21 +0100

    Rechange support files

--- tests/_support/ApiTester.php
+++ tests/_support/ApiTester.php
@@ -2,8 +2,7 @@
 
 
 /**
- * Inherited Methods.
- *
+ * Inherited Methods
  * @method void wantToTest($text)
  * @method void wantTo($text)
  * @method void execute($callable)
@@ -16,12 +15,12 @@
  * @method \Codeception\Lib\Friend haveFriend($name, $actorClass = NULL)
  *
  * @SuppressWarnings(PHPMD)
- */
+*/
 class ApiTester extends \Codeception\Actor
 {
     use _generated\ApiTesterActions;
 
-    /*
-     * Define custom actions here
-     */
+   /**
+    * Define custom actions here
+    */
 }
