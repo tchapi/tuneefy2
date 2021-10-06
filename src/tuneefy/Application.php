bf23ada3f4e6bf33e6e5a4c97fac8e5d2ceaa362
tchapi <regbasket+github@gmail.com>
2021-10-07 00:22:13 +0200

    Slim4: Refactor Not Found handler

--- src/tuneefy/Application.php
+++ src/tuneefy/Application.php
@@ -5,9 +5,11 @@ namespace tuneefy;
 use Chadicus\Slim\OAuth2\Middleware;
 use Chadicus\Slim\OAuth2\Routes;
 use OAuth2;
-use Slim\App;
+use Slim\Exception\HttpNotFoundException;
+use Slim\Factory\AppFactory;
+use Slim\Routing\RouteCollectorProxy;
 use Slim\Views\Twig;
-use Slim\Views\TwigExtension;
+use Slim\Views\TwigMiddleware;
 use Symfony\Bridge\Twig\Extension\TranslationExtension;
 use Symfony\Component\Translation\Loader\YamlFileLoader;
 use Symfony\Component\Translation\Translator;
@@ -45,60 +47,56 @@ class Application
 
     public function __construct()
     {
-        $this->slimApp = new App();
+        $container = new \DI\Container();
 
-        // Get container
-        $container = $this->slimApp->getContainer();
+        AppFactory::setContainer($container);
+        $this->slimApp = AppFactory::create();
 
-        // Register component on container
-        $container['view'] = function ($container) {
-            $view = new Twig(self::getPath('templates'), [
-                //'cache' => self::getPath('cache'),
-            ]);
+        $twig = Twig::create(self::getPath('templates'), [
+          'cache' => false,
+          //'cache' => self::getPath('cache'),
+        ]);
 
-            $basePath = rtrim(str_ireplace('index.php', '', $container['request']->getUri()->getBasePath()), '/');
-            $view->addExtension(new TwigExtension($container['router'], $basePath));
+        // First param is the "default language" to use.
+        if (isset($_COOKIE[self::COOKIE_LANG])) {
+            $locale = $_COOKIE[self::COOKIE_LANG];
+        } elseif (isset($_SERVER['HTTP_ACCEPT_LANGUAGE'])) {
+            $prefLocales = array_reduce(explode(',', $_SERVER['HTTP_ACCEPT_LANGUAGE']), function ($res, $el) {
+                list($l, $q) = array_merge(explode(';q=', $el), [1]);
+                $res[$l] = (float) $q;
 
-            // First param is the "default language" to use.
-            if (isset($_COOKIE[self::COOKIE_LANG])) {
-                $locale = $_COOKIE[self::COOKIE_LANG];
-            } elseif (isset($_SERVER['HTTP_ACCEPT_LANGUAGE'])) {
-                $prefLocales = array_reduce(explode(',', $_SERVER['HTTP_ACCEPT_LANGUAGE']), function ($res, $el) {
-                    list($l, $q) = array_merge(explode(';q=', $el), [1]);
-                    $res[$l] = (float) $q;
+                return $res;
+            }, []);
+            asort($prefLocales);
 
-                    return $res;
-                }, []);
-                asort($prefLocales);
+            $locale = array_reduce(array_keys($prefLocales), function ($default, $prefLocale) {
+                return in_array($prefLocale, ['en_US', 'fr_FR']) ? $prefLocale : $default;
+            }, 'en_US');
+        } else {
+            $locale = 'en_US';
+        }
 
-                $locale = array_reduce(array_keys($prefLocales), function ($default, $prefLocale) {
-                    return in_array($prefLocale, ['en_US', 'fr_FR']) ? $prefLocale : $default;
-                }, 'en_US');
-            } else {
-                $locale = 'en_US';
-            }
+        $translator = new Translator($locale);
+        $twig->getEnvironment()->addGlobal('locale', $locale);
 
-            $translator = new Translator($locale);
-            $view->getEnvironment()->addGlobal('locale', $locale);
+        // If we want to add specific data to the context, it's here
+        $twig->getEnvironment()->addGlobal('context', [
+            // ** Detect Slack
+            'slack' => isset($_SERVER['HTTP_USER_AGENT']) ? (0 !== preg_match('/Slackbot/', $_SERVER['HTTP_USER_AGENT'])) : false,
+        ]);
 
-            // If we want to add specific data to the context, it's here
-            $view->getEnvironment()->addGlobal('context', [
-                'slack' => isset($_SERVER['HTTP_USER_AGENT']) ? (0 !== preg_match('/Slackbot/', $_SERVER['HTTP_USER_AGENT'])) : false,  // ** Detect Slack
-            ]);
+        // Set a fallback language in case you don't have a translation in the default language
+        $translator->setFallbackLocales(['en_US']);
+        $translator->addLoader('yaml', new YamlFileLoader());
 
-            // Set a fallback language incase you don't have a translation in the default language
-            $translator->setFallbackLocales(['en_US']);
-            $translator->addLoader('yaml', new YamlFileLoader());
+        // Add language files here
+        $translator->addResource('yaml', self::getPath('langs').'/fr_FR.yml', 'fr_FR'); // French
+        $translator->addResource('yaml', self::getPath('langs').'/en_US.yml', 'en_US'); // English
 
-            // Add language files here
-            $translator->addResource('yaml', self::getPath('langs').'/fr_FR.yml', 'fr_FR'); // French
-            $translator->addResource('yaml', self::getPath('langs').'/en_US.yml', 'en_US'); // English
+        // add translator functions to Twig
+        $twig->addExtension(new TranslationExtension($translator));
 
-            // add translator functions to Twig
-            $view->addExtension(new TranslationExtension($translator));
-
-            return $view;
-        };
+        $this->slimApp->add(TwigMiddleware::create($this->slimApp, $twig));
 
         // $container['errorHandler'] = function ($container) {
         //     return new CustomErrorHandler($container->get('view'), 500, 'GENERAL_ERROR');
@@ -168,7 +166,7 @@ class Application
         }
 
         $this->container = $this->slimApp->getContainer();
-        $this->container['params'] = $this->params;
+        $this->container->set('params', $this->params);
 
         /* Add session (for bypassing OAuth) */
         $this->slimApp->add(new \Slim\Middleware\Session([
@@ -184,27 +182,18 @@ class Application
 
     public function setupV2ApiRoutes()
     {
-        // Custom 404, 405 and 500 handlers
-        // Override the default handlers
-        $this->container['notFoundHandler'] = function ($container) {
-            return new CustomNotFoundHandler($container->get('view'), true, 404, 'NOT_FOUND');
-        };
-        $this->container['notAllowedHandler'] = function ($container) {
-            return new CustomNotFoundHandler($container->get('view'), true, 405, 'NOT_ALLOWED');
-        };
-
         /* Documentation for the API, has to go first */
         $this->slimApp->get('/', FrontendController::class.':api')->setName('api');
 
         /* The API group, behind an (optional) OAuth2 Server */
-        $api = $this->slimApp->group('/v2', function () {
-            $this->get('/', ApiController::class.':redirect');
-            $this->get('/platforms', ApiController::class.':getAllPlatforms');
-            $this->get('/platform/{tag}', ApiController::class.':getPlatform');
-            $this->get('/lookup', ApiController::class.':lookup');
-            $this->get('/search/{type}/{platform_str}', ApiController::class.':search');
-            $this->get('/aggregate/{type}', ApiController::class.':aggregate');
-            $this->get('/share/{intent}', ApiController::class.':share');
+        $api = $this->slimApp->group('/v2', function (RouteCollectorProxy $app) {
+            $app->get('/', ApiController::class.':redirect');
+            $app->get('/platforms', ApiController::class.':getAllPlatforms');
+            $app->get('/platform/{tag}', ApiController::class.':getPlatform');
+            $app->get('/lookup', ApiController::class.':lookup');
+            $app->get('/search/{type}/{platform_str}', ApiController::class.':search');
+            $app->get('/aggregate/{type}', ApiController::class.':aggregate');
+            $app->get('/share/{intent}', ApiController::class.':share');
         });
 
         if (true === $this->params['api']['use_oauth']) {
@@ -231,19 +220,14 @@ class Application
         }
 
         $this->slimApp->add(new ContentTypeMiddleware($this->container));
+
+        $errorMiddleware = $this->slimApp->addErrorMiddleware(true, true, true);
+        $errorMiddleware->setErrorHandler(HttpNotFoundException::class, new CustomNotFoundHandler(true));
+        $errorMiddleware->setErrorHandler(HttpMethodNotAllowedException::class, new CustomNotFoundHandler(true));
     }
 
     public function setupWebsiteRoutes()
     {
-        // Custom 404, 405 and 500 handlers
-        // Override the default handlers
-        $this->container['notFoundHandler'] = function ($container) {
-            return new CustomNotFoundHandler($container->get('view'), false, 404, 'NOT_FOUND');
-        };
-        $this->container['notAllowedHandler'] = function ($container) {
-            return new CustomNotFoundHandler($container->get('view'), false, 405, 'NOT_ALLOWED');
-        };
-
         /* The display/show page for a musical entity */
         $this->slimApp->get($this->params['urls']['format'], FrontendController::class.':show')->setName('show');
         $this->slimApp->get('/a/{uid}', FrontendController::class.':show')->setName('legacy_show_album');
@@ -261,19 +245,23 @@ class Application
         $this->slimApp->get('/rate-limiting', FrontendController::class.':apiRateLimiting');
 
         /* The backend routes */
-        $admin = $this->slimApp->group('/admin', function () {
-            $this->get('/migrate', BackendController::class.':migrate')->setName('admin_migrate');
-            $this->get('/dashboard', BackendController::class.':dashboard')->setName('admin_dashboard');
-            $this->get('/api/clients', BackendController::class.':clients')->setName('admin_clients');
-            $this->map(['GET', 'POST'], '/api/clients/new', BackendController::class.':createClient')->setName('admin_new_client');
+        $admin = $this->slimApp->group('/admin', function (RouteCollectorProxy $app) {
+            $app->get('/migrate', BackendController::class.':migrate')->setName('admin_migrate');
+            $app->get('/dashboard', BackendController::class.':dashboard')->setName('admin_dashboard');
+            $app->get('/api/clients', BackendController::class.':clients')->setName('admin_clients');
+            $app->map(['GET', 'POST'], '/api/clients/new', BackendController::class.':createClient')->setName('admin_new_client');
         });
 
         /* Middlewares should be in a certain order, the authentication must be added last */
-        $this->slimApp->add(new \Slim\Middleware\HttpBasicAuthentication([
+        $this->slimApp->add(new \Tuupola\Middleware\HttpBasicAuthentication([
             'path' => ['/admin'],
             'realm' => 'Protected access',
             'secure' => $this->params['admin_secure'],
             'users' => $this->params['admin_users'],
         ]));
+
+        $errorMiddleware = $this->slimApp->addErrorMiddleware(true, true, true);
+        $errorMiddleware->setErrorHandler(HttpNotFoundException::class, new CustomNotFoundHandler(false));
+        $errorMiddleware->setErrorHandler(HttpMethodNotAllowedException::class, new CustomNotFoundHandler(false));
     }
 }
--- src/tuneefy/Utils/CustomNotFoundHandler.php
+++ src/tuneefy/Utils/CustomNotFoundHandler.php
@@ -5,42 +5,39 @@ namespace tuneefy\Utils;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use RKA\ContentTypeRenderer\Renderer;
-use Slim\Http\Response;
+use Slim\Interfaces\ErrorHandlerInterface;
+use Slim\Psr7\Response;
 use Slim\Views\Twig;
 use tuneefy\Controller\ApiController;
 
-class CustomNotFoundHandler
+class CustomNotFoundHandler implements ErrorHandlerInterface
 {
-    private $status;
-    private $message;
+    private bool $isApiRoute;
 
-    private $view;
-
-    private $renderer;
-
-    public function __construct(Twig $view, bool $isApiRoute, int $status, string $message)
+    public function __construct(bool $isApiRoute)
     {
         $this->isApiRoute = $isApiRoute;
-        $this->view = $view;
-        $this->status = $status;
-        $this->message = $message;
-
-        $this->renderer = new Renderer();
     }
 
-    public function __invoke(ServerRequestInterface $request, ResponseInterface $response)
+    public function __invoke(ServerRequestInterface $request, \Throwable $exception, bool $displayErrorDetails, bool $logErrors, bool $logErrorDetails): ResponseInterface
     {
+        $status = $exception->getCode();
+        $response = new Response();
+
         // Depending on the group we should render an error page or a structured response
         if (!$this->isApiRoute) {
-            $response = $response->withStatus($this->status)
+            $response = $response->withStatus($status)
                                  ->withHeader('Content-Type', 'text/html; charset=UTF-8');
 
-            return $this->view->render($response, '404.html.twig');
+            $twig = Twig::fromRequest($request);
+            return $twig->render($response, '404.html.twig');
         } else {
-            $response->withStatus($this->status);
+            $response->withStatus($status);
 
-            return $this->renderer->render($request, $response, [
-                'errors' => [ApiController::ERRORS[$this->message]],
+            $renderer = new Renderer();
+
+            return $renderer->render($request, $response, [
+                'errors' => [ApiController::ERRORS[$status]],
             ]);
         }
     }
