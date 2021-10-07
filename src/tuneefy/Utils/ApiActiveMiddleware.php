2d5bb5c736e085680f1de909fd034fd02dd6ede5
tchapi <regbasket+github@gmail.com>
2021-10-07 21:25:56 +0200

    Fix typo in middleware

--- src/tuneefy/Utils/ApiActiveMiddleware.php
+++ src/tuneefy/Utils/ApiActiveMiddleware.php
@@ -4,7 +4,6 @@ namespace tuneefy\Utils;
 
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
-
 use RKA\ContentTypeRenderer\Renderer;
 use Slim\Psr7\Response;
 use tuneefy\Controller\ApiController;
@@ -21,7 +20,7 @@ class ApiActiveMiddleware
     public function __invoke(Request $request, RequestHandler $handler): Response
     {
         $method = $this->container->has('api_method') ? $this->container->get('api_method') : null;
-        $clientId = $this->container->has('token') ? $this->container[->get('token')['client_id'] : null;
+        $clientId = $this->container->has('token') ? $this->container->get('token')['client_id'] : null;
 
         if (null !== $clientId) { // If we're using oauth, else it's unecessary
             $db = DatabaseHandler::getInstance(null);
